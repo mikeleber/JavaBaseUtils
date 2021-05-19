956a5c0fb0899844b1c09b9ef5fa657d37043d43
mike_ <mikeleber71@googlemail.com>
2021-05-19 06:57:10 +0200

    Add proxy to http client

--- Net/src/main/java/SimpleSoap.java
+++ Net/src/main/java/SimpleSoap.java
@@ -1,8 +1,10 @@
+import org.apache.http.HttpHost;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
@@ -17,12 +19,13 @@ public class SimpleSoap {
         //wsdl file :http://currencyconverter.kowabunga.net/converter.asmx?wsdl
         InputStream soapRequestFile = org.basetools.util.io.FileUtils.streamFromClassloader(getClass(), "SoapRequestFile.xml");
 
-        CloseableHttpClient client = HttpClients.createDefault(); //create client
+        CloseableHttpClient client = getCloseableHttpClient();
         HttpPost request = new HttpPost(soapEndpoint); //Create the request
         request.addHeader("Content-Type", "text/xml"); //adding header
         if (strSoapAction != null) {
             request.addHeader("SOAPAction", strSoapAction);
         }
+
         request.setEntity(new InputStreamEntity(soapRequestFile));
         CloseableHttpResponse response = client.execute(request);//Execute the command
 
@@ -31,5 +34,21 @@ public class SimpleSoap {
 
         String responseString = EntityUtils.toString(response.getEntity(), "UTF-8");//Getting the Response body
         System.out.println(responseString);
+    }
+
+    private CloseableHttpClient getCloseableHttpClient() {
+        HttpClientBuilder clientbuilder = HttpClientBuilder.create(); //create client
+        // Set HTTP proxy, if specified in system properties
+        if (System.getProperty("http.proxyHost") != null) {
+            int port = 80;
+            if (System.getProperty("http.proxyPort") != null) {
+                port = Integer.parseInt(System.getProperty("http.proxyPort"));
+            }
+            HttpHost proxy = new HttpHost(System.getProperty("http.proxyHost"), port, "http");
+            DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
+            clientbuilder.setRoutePlanner(routePlanner);
+        }
+        CloseableHttpClient client = clientbuilder.build();
+        return client;
     }
 }
