4c23d1982cc96311d83869ef0d4aa7a546f01449
mike_ <mikeleber71@googlemail.com>
2021-05-19 08:00:36 +0200

    EmptyUtil

---
+++ src/main/java/org/basetools/util/EmptyUtil.java
@@ -0,0 +1,25 @@
+package org.basetools.util;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class EmptyUtil {
+    public static final boolean isEmpty(String x) {
+        return (x == null || x.trim().equals(""));
+    }
+    public static final boolean isEmpty(Integer x) {
+        return (x == null || x == 0);
+    }
+    public static final boolean isEmpty(Long x) {
+        return (x == null || x == 0);
+    }
+    public static final boolean isEmpty(Collection<?> x) {
+        return (x == null || x.size() == 0);
+    }
+    public static final boolean isEmpty(Map<?,?> x) {
+        return (x == null || x.size() == 0);
+    }
+    public static final boolean isEmpty(Object[] x) {
+        return (x == null || x.length == 0);
+    }
+}
