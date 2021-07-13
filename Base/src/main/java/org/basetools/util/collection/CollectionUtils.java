5abce1eacbe491c437b0641898ecd68557fccdc8
Mike <mike_leber@yahoo.com>
2021-07-13 17:16:52 +0200

    Fix xpathengine and multiselect

--- Base/src/main/java/org/basetools/util/collection/CollectionUtils.java
+++ Base/src/main/java/org/basetools/util/collection/CollectionUtils.java
@@ -132,78 +132,6 @@ public class CollectionUtils {
         list.add(obj);
     }
 
-    /**
-     * Returns true if vectA and vectB have any elements in common.
-     */
-    public static <T> boolean overlaps(List <T>listA, List <T>listB) {
-        if (listA == null || listB == null || listA.size() == 0 || listB.size() == 0) {
-            return false;
-        }
-        if ((listA.size() + listB.size()) > 20) {
-            Hashtable workSet = new Hashtable(listA.size());
-            for (int i = 0; i < listA.size(); i++) {
-                workSet.put(listA.get(i), listA.get(i));
-            }
-            for (int i = 0; i < listB.size(); i++) {
-                if (workSet.containsKey(listB.get(i))) {
-                    return true;
-                }
-            }
-        } else {
-            if (listA.size() > listB.size()) {
-                for (int i = 0; i < listA.size(); i++) {
-                    if (listB.contains(listA.get(i))) {
-                        return true;
-                    }
-                }
-            } else {
-                for (int i = 0; i < listB.size(); i++) {
-                    if (listA.contains(listB.get(i))) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-
-    public static<T> List<T> intersection(List<T> vectA, List<T> listB) {
-        Hashtable workSetA = new Hashtable(), workSetB = new Hashtable(), resultSet = new Hashtable();
-        Vector result = new Vector();
-        Enumeration aTempEnum;
-        Object item;
-        /* -- */
-        if (vectA != null) {
-            for (Object o : vectA) {
-                workSetA.put(o, o);
-            }
-        }
-        if (listB != null) {
-            for (Object o : listB) {
-                workSetA.put(o, o);
-            }
-        }
-        aTempEnum = workSetA.elements();
-        while (aTempEnum.hasMoreElements()) {
-            item = aTempEnum.nextElement();
-            if (workSetB.containsKey(item)) {
-                resultSet.put(item, item);
-            }
-        }
-        aTempEnum = workSetB.elements();
-        while (aTempEnum.hasMoreElements()) {
-            item = aTempEnum.nextElement();
-            if (workSetA.containsKey(item)) {
-                resultSet.put(item, item);
-            }
-        }
-        aTempEnum = resultSet.elements();
-        while (aTempEnum.hasMoreElements()) {
-            result.addElement(aTempEnum.nextElement());
-        }
-        return result;
-    }
 
     public static <T> T getFirstCommonObject(List<T> listA, List<T> listB) {
         if (listA != null && listB != null && listB.size() > 0) {
@@ -215,156 +143,7 @@ public class CollectionUtils {
         return null;
     }
 
-    /**
-     * This method returns a Vector containing the set of objects contained in vectA that are not contained in vectB. This method will always return a new, non-null Vector, even if vectA and/or vectB are null.
-     */
-    public static Vector difference(Vector vectA, Vector vectB) {
-        Vector result = new Vector();
-        Object item;
-        /* -- */
-        if (vectA == null) {
-            return result;
-        }
-        if (vectB == null) {
-            return (Vector) vectA.clone();
-        }
-        if (vectA.size() + vectB.size() > 10) // ass
-        {
-            Hashtable workSetB = new Hashtable();
-            Enumeration aTempEnum;
-            /* -- */
-            aTempEnum = vectB.elements();
-            while (aTempEnum.hasMoreElements()) {
-                item = aTempEnum.nextElement();
-                workSetB.put(item, item);
-            }
-            aTempEnum = vectA.elements();
-            while (aTempEnum.hasMoreElements()) {
-                item = aTempEnum.nextElement();
-                if (!workSetB.containsKey(item)) {
-                    result.addElement(item);
-                }
-            }
-        } else {
-            for (int i = 0; i < vectA.size(); i++) {
-                item = vectA.elementAt(i);
-                if (!vectB.contains(item)) {
-                    result.addElement(item);
-                }
-            }
-        }
-        return result;
-    }
 
-    public static List compareDifference(Collection vectA, Collection vectB) {
-        Collection tempA = vectA, tempB = vectB;
-        if (vectA != null && vectB != null) {
-            if (vectB.size() > vectA.size()) {
-                tempA = vectB;
-                tempB = vectA;
-            }
-        }
-        return difference(tempA, tempB);
-    }
-
-    public static List compareSequence(List vectA, List vectB) {
-        List tempA = vectA, tempB = vectB;
-        if (vectA != null && vectB != null) {
-            if (vectB.size() > vectA.size()) {
-                tempA = vectB;
-                tempB = vectA;
-            }
-        }
-        return sameSequence(tempA, tempB);
-    }
-
-    /**
-     * This method returns a Collection containing the set of objects contained in vectA that are not contained in vectB. This method will always return a new, non-null List , even if vectA and/or vectB are null.
-     */
-    public static List difference(Collection vectA, Collection vectB) {
-        Object item;
-        /* -- */
-        Collection temp = null;
-        if (vectA != null && vectB != null && vectB.size() > vectA.size()) {
-            temp = vectA;
-            vectA = vectB;
-            vectB = temp;
-        }
-        if (vectA == null) {
-            return new ArrayList(0);
-        }
-        if (vectB == null) {
-            return Arrays.asList(vectA.toArray());
-        }
-        ArrayList result = new ArrayList(vectA.size());
-        if (vectA.size() + vectB.size() > 10) // ass
-        {
-            HashMap workSetB = new HashMap();
-            Iterator aTempEnum;
-            /* -- */
-            aTempEnum = vectB.iterator();
-            while (aTempEnum.hasNext()) {
-                item = aTempEnum.next();
-                workSetB.put(item, item);
-            }
-            aTempEnum = vectA.iterator();
-            while (aTempEnum.hasNext()) {
-                item = aTempEnum.next();
-                if (!workSetB.containsKey(item)) {
-                    result.add(item);
-                }
-            }
-        } else {
-            Iterator aTempEnum;
-            /* -- */
-            aTempEnum = vectA.iterator();
-            while (aTempEnum.hasNext()) {
-                item = aTempEnum.next();
-                if (!vectB.contains(item)) {
-                    result.add(item);
-                }
-            }
-        }
-        return result;
-    }
-
-    /**
-     * This method returns a Vector of items that appeared in the vector parameter more than once. If no duplicates are found or if vector is null, this method returns null.
-     */
-    public static Vector duplicates(Vector vector) {
-        if (vector == null) {
-            return null;
-        }
-        Vector result = null;
-        Hashtable found = new Hashtable();
-        for (int i = 0; i < vector.size(); i++) {
-            Object item = vector.elementAt(i);
-            if (found.containsKey(item)) {
-                if (result == null) {
-                    result = new Vector();
-                }
-                unionAdd(result, item);
-            }
-            found.put(item, item);
-        }
-        return result;
-    }
-
-
-    public static <T> List<T> removeDuplicates2(List<T> aList) {
-        if (aList == null) {
-            return null;
-        }
-        if (aList.size() <= 1) {
-            return aList;
-        }
-        HashSet<T> h = new HashSet<T>(aList);
-        if (h.size() != aList.size()) {
-            aList.clear();
-            aList.addAll(h);
-        }
-        return aList;
-    }
 
     public static List keepEvery(List array, int every) {
         if (array == null || array.size() == 0 || every == 1) {
@@ -439,39 +218,7 @@ public class CollectionUtils {
     }
 
 
-    public static <T> List<T> sameSequence(List <T>a, List<T> b) {
-        if (a == null || b == null) {
-            return null;
-        }
-        ArrayList<T> result = null;
-        int bSize = b.size();
-        int aSize = a.size();
-        for (int i = 0; i < aSize; i++) {
-            Object itemA = a.get(i);
-            Object itemB = null;
-            if (i < bSize && (b.get(i) == itemA || (itemB != null && itemB.equals(itemA)))) {
-                // everything is ok!
-            } else {
-                if (result == null) {
-                    result = new ArrayList<T> ();
-                }
-                result.add((T)itemB);
-            }
-        }
-        if (bSize > aSize) {
-            for (int i = aSize; i < bSize; i++) {
-                Object itemB = b.get(i);
-                if (result == null) {
-                    result = new ArrayList<T>();
-                }
-                result.add((T)itemB);
-            }
-        }
-        if (result == null) {
-            result = new ArrayList<T>();
-        }
-        return result;
-    }
+
 
     /**
      * This method returns a Vector containing the elements of vectA minus the elements of vectB. If vectA has an element in the Vector 5 times and vectB has it 3 times, the result will have it two times. This method will always return a new, non-null Vector, even if vectA and/or vectB are null.
@@ -500,19 +247,6 @@ public class CollectionUtils {
 
 
 
-    public static boolean isSame(List listA, List listB) {
-        if (listA.size() != listB.size()) {
-            return false;
-        }
-        for (int a = 0; a < listA.size(); a++) {
-            Object oa = listA.get(a);
-            Object ob = listB.get(a);
-            if (!(oa == ob || oa != null && oa.equals(ob))) {
-                return false;
-            }
-        }
-        return true;
-    }
 
     /**
      * This method takes a sepChars-separated string and converts it to a vector of fields. i.e., "gomod,jonabbey" -> a vector whose elements are "gomod" and "jonabbey". NOTE: this method will omit 'degenerate' fields from the output vector. That is, if input is "gomod,,, jonabbey" and sepChars is
@@ -578,75 +312,7 @@ public class CollectionUtils {
         }
     }
 
-    public static void main(String[] args) {
-        // String testString = "jon, beth ross,,,darren,anna";
-        // String testString = "jon, beth ross,,,darren,anna,,,,,";
-        // Vector results = stringVector(testString, ", ");
-        // for (int i = 0; i < results.size(); i++) {
-        // System.out.println(i + ": " + results.elementAt(i));
-        String[] testa = new String[] { "1" };
-        String[] test2 = new String[] { "1" };
-        System.out.println(testa.equals(test2));
-        // }
-        Vector a = new Vector();
-        a.add("1");
-        a.add("2");
-        a.add("3");
-        a.add("4");
-        a.add("5");
-        a.add("8");
-        a.add("6");
-        a.add("7");
-        a.add("8");
-        a.add("9");
-        a.add("10");
-        a.add("11");
-        // removeDuplicatesLastWins2(a);
-        sublist(a, a.get(3), true);
-        removeLast(a, 3);
-        keepEvery(a, 4);
-        removeDuplicatesLastWins(a);
-        Vector b = new Vector();
-        b.add("a");
-        b.add("b");
-        b.add("c");
-        b.add("d");
-        sameSequence(a, b);
-        Vector as = new Vector();
-        as.add("a");
-        as.add("b");
-        as.add("c");
-        ;
-        b = new Vector();
-        b.add("a");
-        b.add("b");
-        b.add("c");
-        b.add("d");
-        sameSequence(as, b);
-        a = new Vector();
-        a.add("a");
-        a.add("b");
-        a.add("c");
-        a.add("d");
-        Vector bs = new Vector();
-        bs.add("a");
-        bs.add("b");
-        bs.add("c");
-        bs.add(null);
-        System.out.println(sameSequence(a, bs).stream().collect(
-                Collectors.joining(",")));
-        Vector an = new Vector();
-        an.add("a");
-        an.add(null);
-        an.add("c");
-        an.add("d");
-        Vector bn = new Vector();
-        bn.add("a");
-        bn.add("b");
-        bn.add("c");
-        System.out.println(sameSequence(an, bn).stream().collect(
-                Collectors.joining(",")));
-    }
+
 
     public static List getStartsWith(List array, String sword) {
         List result = new ArrayList(5);
---
+++ Base/src/main/java/org/basetools/util/compare/CollectionCompareUtils.java
@@ -0,0 +1,267 @@
+package org.basetools.util.compare;
+
+import org.basetools.util.array.ArrayUtil;
+import org.basetools.util.collection.CollectionUtils;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CollectionCompareUtils {
+
+    /**
+     * Returns true if vectA and vectB have any elements in common.
+     */
+    public static <T> boolean overlaps(List<T> listA, List<T> listB) {
+        if (listA == null || listB == null || listA.size() == 0 || listB.size() == 0) {
+            return false;
+        }
+        if ((listA.size() + listB.size()) > 20) {
+            Hashtable workSet = new Hashtable(listA.size());
+            for (int i = 0; i < listA.size(); i++) {
+                workSet.put(listA.get(i), listA.get(i));
+            }
+            for (int i = 0; i < listB.size(); i++) {
+                if (workSet.containsKey(listB.get(i))) {
+                    return true;
+                }
+            }
+        } else {
+            if (listA.size() > listB.size()) {
+                for (int i = 0; i < listA.size(); i++) {
+                    if (listB.contains(listA.get(i))) {
+                        return true;
+                    }
+                }
+            } else {
+                for (int i = 0; i < listB.size(); i++) {
+                    if (listA.contains(listB.get(i))) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static <T> List<T> intersection(List<T> vectA, List<T> listB) {
+        Hashtable workSetA = new Hashtable(), workSetB = new Hashtable(), resultSet = new Hashtable();
+        Vector result = new Vector();
+        Enumeration aTempEnum;
+        Object item;
+        /* -- */
+        if (vectA != null) {
+            for (Object o : vectA) {
+                workSetA.put(o, o);
+            }
+        }
+        if (listB != null) {
+            for (Object o : listB) {
+                workSetA.put(o, o);
+            }
+        }
+        aTempEnum = workSetA.elements();
+        while (aTempEnum.hasMoreElements()) {
+            item = aTempEnum.nextElement();
+            if (workSetB.containsKey(item)) {
+                resultSet.put(item, item);
+            }
+        }
+        aTempEnum = workSetB.elements();
+        while (aTempEnum.hasMoreElements()) {
+            item = aTempEnum.nextElement();
+            if (workSetA.containsKey(item)) {
+                resultSet.put(item, item);
+            }
+        }
+        aTempEnum = resultSet.elements();
+        while (aTempEnum.hasMoreElements()) {
+            result.addElement(aTempEnum.nextElement());
+        }
+        return result;
+    }
+
+
+    /**
+     * This method returns a Vector containing the set of objects contained in vectA that are not contained in vectB. This method will always return a new, non-null Vector, even if vectA and/or vectB are null.
+     */
+    public static Vector difference(Vector vectA, Vector vectB) {
+        Vector result = new Vector();
+        Object item;
+        /* -- */
+        if (vectA == null) {
+            return result;
+        }
+        if (vectB == null) {
+            return (Vector) vectA.clone();
+        }
+        if (vectA.size() + vectB.size() > 10) // ass
+        {
+            Hashtable workSetB = new Hashtable();
+            Enumeration aTempEnum;
+            /* -- */
+            aTempEnum = vectB.elements();
+            while (aTempEnum.hasMoreElements()) {
+                item = aTempEnum.nextElement();
+                workSetB.put(item, item);
+            }
+            aTempEnum = vectA.elements();
+            while (aTempEnum.hasMoreElements()) {
+                item = aTempEnum.nextElement();
+                if (!workSetB.containsKey(item)) {
+                    result.addElement(item);
+                }
+            }
+        } else {
+            for (int i = 0; i < vectA.size(); i++) {
+                item = vectA.elementAt(i);
+                if (!vectB.contains(item)) {
+                    result.addElement(item);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static List compareDifference(Collection vectA, Collection vectB) {
+        Collection tempA = vectA, tempB = vectB;
+        if (vectA != null && vectB != null) {
+            if (vectB.size() > vectA.size()) {
+                tempA = vectB;
+                tempB = vectA;
+            }
+        }
+        return difference(tempA, tempB);
+    }
+
+    public static List compareSequence(List vectA, List vectB) {
+        List tempA = vectA, tempB = vectB;
+        if (vectA != null && vectB != null) {
+            if (vectB.size() > vectA.size()) {
+                tempA = vectB;
+                tempB = vectA;
+            }
+        }
+        return sameSequence(tempA, tempB);
+    }
+
+    /**
+     * This method returns a Collection containing the set of objects contained in vectA that are not contained in vectB. This method will always return a new, non-null List , even if vectA and/or vectB are null.
+     */
+    public static List difference(Collection vectA, Collection vectB) {
+        Object item;
+        /* -- */
+        Collection temp = null;
+        if (vectA != null && vectB != null && vectB.size() > vectA.size()) {
+            temp = vectA;
+            vectA = vectB;
+            vectB = temp;
+        }
+        if (vectA == null) {
+            return new ArrayList(0);
+        }
+        if (vectB == null) {
+            return Arrays.asList(vectA.toArray());
+        }
+        ArrayList result = new ArrayList(vectA.size());
+        if (vectA.size() + vectB.size() > 10) // ass
+        {
+            HashMap workSetB = new HashMap();
+            Iterator aTempEnum;
+            /* -- */
+            aTempEnum = vectB.iterator();
+            while (aTempEnum.hasNext()) {
+                item = aTempEnum.next();
+                workSetB.put(item, item);
+            }
+            aTempEnum = vectA.iterator();
+            while (aTempEnum.hasNext()) {
+                item = aTempEnum.next();
+                if (!workSetB.containsKey(item)) {
+                    result.add(item);
+                }
+            }
+        } else {
+            Iterator aTempEnum;
+            /* -- */
+            aTempEnum = vectA.iterator();
+            while (aTempEnum.hasNext()) {
+                item = aTempEnum.next();
+                if (!vectB.contains(item)) {
+                    result.add(item);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * This method returns a Vector of items that appeared in the vector parameter more than once. If no duplicates are found or if vector is null, this method returns null.
+     */
+    public static Vector duplicates(Vector vector) {
+        if (vector == null) {
+            return null;
+        }
+        Vector result = null;
+        Hashtable found = new Hashtable();
+        for (int i = 0; i < vector.size(); i++) {
+            Object item = vector.elementAt(i);
+            if (found.containsKey(item)) {
+                if (result == null) {
+                    result = new Vector();
+                }
+                CollectionUtils.unionAdd(result, item);
+            }
+            found.put(item, item);
+        }
+        return result;
+    }
+
+    public static <T> List<T> sameSequence(List<T> a, List<T> b) {
+        if (a == null || b == null) {
+            return null;
+        }
+        ArrayList<T> result = null;
+        int bSize = b.size();
+        int aSize = a.size();
+        for (int i = 0; i < aSize; i++) {
+            Object itemA = a.get(i);
+            Object itemB = null;
+            if (i < bSize && (b.get(i) == itemA || (itemB != null && itemB.equals(itemA)))) {
+                // everything is ok!
+            } else {
+                if (result == null) {
+                    result = new ArrayList<T>();
+                }
+                result.add((T) itemB);
+            }
+        }
+        if (bSize > aSize) {
+            for (int i = aSize; i < bSize; i++) {
+                Object itemB = b.get(i);
+                if (result == null) {
+                    result = new ArrayList<T>();
+                }
+                result.add((T) itemB);
+            }
+        }
+        if (result == null) {
+            result = new ArrayList<T>();
+        }
+        return result;
+    }
+
+    public static boolean isSame(List listA, List listB) {
+        if (listA.size() != listB.size()) {
+            return false;
+        }
+        for (int a = 0; a < listA.size(); a++) {
+            Object oa = listA.get(a);
+            Object ob = listB.get(a);
+            if (!(oa == ob || oa != null && oa.equals(ob))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
