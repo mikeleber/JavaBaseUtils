40b00347e3954613773e461fc0f2b680c25b7d04
Mike <mike_leber@yahoo.com>
2022-05-31 20:06:23 +0200

    json schema

--- Base/src/main/java/org/basetools/util/StringUtils.java
+++ Base/src/main/java/org/basetools/util/StringUtils.java
@@ -70,6 +70,61 @@ public class StringUtils {
         return string != null && string.trim().startsWith("<") && string.trim().endsWith(">");
     }
 
+    public static ContentType getDataType(String string) {
+        if (string == null) return ContentType.unknown;
+        String content = string.trim();
+        if (content.startsWith("<") && content.endsWith(">")) return ContentType.xml;
+        if (content.startsWith("{") && content.endsWith("}")) {
+            if (content.indexOf("$schema") >= 0) return ContentType.jsonSchema;
+            else return ContentType.json;
+        }
+
+        return ContentType.unknown;
+    }
+
+    public static boolean regionMatches(final CharSequence cs, final boolean ignoreCase, final int thisStart, final CharSequence substring, final int start, final int length) {
+        if (cs instanceof String && substring instanceof String) {
+            return ((String) cs).regionMatches(ignoreCase, thisStart, (String) substring, start, length);
+        }
+        int index1 = thisStart;
+        int index2 = start;
+        int tmpLen = length;
+
+        // Extract these first so we detect NPEs the same as the java.lang.String version
+        final int srcLen = cs.length() - thisStart;
+        final int otherLen = substring.length() - start;
+
+        // Check for invalid parameters
+        if (thisStart < 0 || start < 0 || length < 0) {
+            return false;
+        }
+
+        // Check that the regions are long enough
+        if (srcLen < length || otherLen < length) {
+            return false;
+        }
+
+        while (tmpLen-- > 0) {
+            final char c1 = cs.charAt(index1++);
+            final char c2 = substring.charAt(index2++);
+
+            if (c1 == c2) {
+                continue;
+            }
+
+            if (!ignoreCase) {
+                return false;
+            }
+
+            // The same check as in String.regionMatches():
+            if (Character.toUpperCase(c1) != Character.toUpperCase(c2) && Character.toLowerCase(c1) != Character.toLowerCase(c2)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     /**
      * @deprecated use Apache Stringutils.stripEnd
      */
@@ -130,49 +185,35 @@ public class StringUtils {
         return regionMatches(str, ignoreCase, 0, prefix, 0, prefix.length());
     }
 
-    public static boolean regionMatches(final CharSequence cs, final boolean ignoreCase, final int thisStart,
-                                        final CharSequence substring, final int start, final int length) {
-        if (cs instanceof String && substring instanceof String) {
-            return ((String) cs).regionMatches(ignoreCase, thisStart, (String) substring, start, length);
-        }
-        int index1 = thisStart;
-        int index2 = start;
-        int tmpLen = length;
+    public static String[] getTokensArray(String strData, String strDelimiters, boolean returnDelims) {
 
-        // Extract these first so we detect NPEs the same as the java.lang.String version
-        final int srcLen = cs.length() - thisStart;
-        final int otherLen = substring.length() - start;
+        String[] strTokenArray = null;
 
-        // Check for invalid parameters
-        if (thisStart < 0 || start < 0 || length < 0) {
-            return false;
-        }
+        try {
 
-        // Check that the regions are long enough
-        if (srcLen < length || otherLen < length) {
-            return false;
-        }
+            if (strData == null || strDelimiters == null) return strTokenArray;
 
-        while (tmpLen-- > 0) {
-            final char c1 = cs.charAt(index1++);
-            final char c2 = substring.charAt(index2++);
+            //create tokenizer object
+            StringTokenizer st = new StringTokenizer(strData, strDelimiters, returnDelims);
 
-            if (c1 == c2) {
-                continue;
+            /*
+             * Create the array having same size as total
+             * number of tokens
+             */
+            strTokenArray = new String[st.countTokens()];
+
+            //iterate through all the tokens
+            int count = 0;
+            while (st.hasMoreTokens()) {
+
+                //add to an array
+                strTokenArray[count++] = st.nextToken();
             }
-
-            if (!ignoreCase) {
-                return false;
-            }
-
-            // The same check as in String.regionMatches():
-            if (Character.toUpperCase(c1) != Character.toUpperCase(c2)
-                    && Character.toLowerCase(c1) != Character.toLowerCase(c2)) {
-                return false;
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return true;
+        return strTokenArray;
     }
 
     public static final String toPath(Stack stack) {
@@ -360,37 +401,7 @@ public class StringUtils {
         return MurmurHash.hash32(s);
     }
 
-    public static String[] getTokensArray(String strData, String strDelimiters, boolean returnDelims) {
-
-        String[] strTokenArray = null;
-
-        try {
-
-            if (strData == null || strDelimiters == null)
-                return strTokenArray;
-
-            //create tokenizer object
-            StringTokenizer st = new StringTokenizer(strData, strDelimiters, returnDelims);
-
-            /*
-             * Create the array having same size as total
-             * number of tokens
-             */
-            strTokenArray = new String[st.countTokens()];
-
-            //iterate through all the tokens
-            int count = 0;
-            while (st.hasMoreTokens()) {
-
-                //add to an array
-                strTokenArray[count++] = st.nextToken();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return strTokenArray;
-    }
+    public enum ContentType {unknown, xml, jsonSchema, json, yaml}
 
     public static final int countChars(CharSequence src, char toCount, boolean retDelims) {
         int numOfElements = 0;
