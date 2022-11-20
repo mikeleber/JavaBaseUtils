b61bdf375607bc102b4866d0dd104115f8555b68
mike_ <mikeleber71@googlemail.com>
2022-11-20 17:20:59 +0100

    Fix codeset null selection on startup

--- Base/src/main/java/org/basetools/util/ByteUtils.java
+++ Base/src/main/java/org/basetools/util/ByteUtils.java
@@ -8,4 +8,28 @@ public class ByteUtils {
     public static byte toByte(int val) {
         return (byte) (val == 1 ? 1 : 0);
     }
+
+    public static final boolean matchesBinary(long what, long with) {
+        return (what & with) != 0;
+    }
+
+    public static final long combineBinary(long cumulated, long with) {
+        return cumulated | with;
+    }
+
+    public static final boolean matchesBinary(int what, int with) {
+        return (what & with) != 0;
+    }
+
+    public static final int combineBinary(int cumulated, int with) {
+        return cumulated | with;
+    }
+
+    public static final boolean matchesBinary(short what, short with) {
+        return (what & with) != 0;
+    }
+
+    public static final short combineBinary(short cumulated, short with) {
+        return (short) (cumulated | with);
+    }
 }
