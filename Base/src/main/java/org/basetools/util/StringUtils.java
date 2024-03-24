4d0671ede7b212a8748ccdcadb56eb13e3f91d9d
Mike <mike_leber@yahoo.com>
2024-03-24 14:35:00 +0100

    add isInteger for StringUtils

--- Base/src/main/java/org/basetools/util/StringUtils.java
+++ Base/src/main/java/org/basetools/util/StringUtils.java
@@ -486,7 +486,29 @@ public class StringUtils {
         if (val == null) return val;
         return val.replaceAll(what, with);
     }
-
+    public static boolean isInteger(String str) {
+        if (str == null) {
+            return false;
+        }
+        int length = str.length();
+        if (length == 0) {
+            return false;
+        }
+        int i = 0;
+        if (str.charAt(0) == '-') {
+            if (length == 1) {
+                return false;
+            }
+            i = 1;
+        }
+        for (; i < length; i++) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
     public enum ContentType {
         unknown, xml, xsd, xjson, json, yaml, csv, txt, nls;
 
