3227505882359e09d8dab54e3294586821022f41
mike_ <mike_leber@yahoo.com>
2024-12-17 22:29:11 +0100

    intToByteString

--- Base/src/main/java/org/basetools/util/ByteUtils.java
+++ Base/src/main/java/org/basetools/util/ByteUtils.java
@@ -19,6 +19,21 @@ public class ByteUtils {
         return mask | value;
     }
 
+    public static String intToByteString(int number, int groupSize) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 31; i >= 0; i--) {
+            int mask = 1 << i;
+            result.append((number & mask) != 0 ? "1" : "0");
+
+            if (i % groupSize == 0)
+                result.append(" ");
+        }
+        result.replace(result.length() - 1, result.length(), "");
+
+        return result.toString();
+    }
+
     public static byte toByte(int val) {
         return (byte) (val == 1 ? 1 : 0);
     }
